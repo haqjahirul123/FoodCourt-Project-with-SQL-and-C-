e965374f33f2d4a08796ec0d3549931db5d2ed5c
Haqjahirul123 <haqjahirul1983@gmail.com>
2021-12-01 10:52:44 +0100

    master

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,21 +1,14 @@
-using DataLayer.Backend;
-using DataLayer.Data;
-
 var builder = WebApplication.CreateBuilder(args);
-var admin = new AdminBackend();
-admin.CreateAndSeedDb();
-using var ctx = new FoodRescue_DbContext();
-Console.WriteLine(ctx);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -27,6 +20,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
