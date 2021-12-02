9368afdee60f1f4d78ffc8bdf1f271950c52d118
Haqjahirul123 <haqjahirul1983@gmail.com>
2021-12-02 11:43:04 +0100

    CSS and HTML  added

---
+++ UserFrontend/Models/Users.cs
@@ -0,0 +1,44 @@
+﻿using DataLayer.Backend;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+namespace UserFrontend.Models
+{
+    public class Users
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string userId { get; set; }
+        public string passWord { get; set; }
+        public string isAdmin { get; set; }
+        public List<Users> GetUsersInfo()
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog= FoodRescue_ProjectDatabase_REAL ; Integrated Security=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            string sqlQuery = "Select FirstName, LastName,Email,UserId, Password, isAdmin From UserPrivateInfos Where UserId =2";
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            //Users users = new Users();
+           List<Users> usersInfoList = new List<Users>();
+
+            while (dr.Read())
+            {
+                Users users = new Users();
+                users.firstName = dr["FirstName"].ToString();
+                users.lastName = dr["lastName"].ToString();
+                users.email = dr["Email"].ToString();
+                users.userId = dr["UserId"].ToString();
+                users.passWord = dr["Password"].ToString();
+                users.isAdmin = dr["isAdmin"].ToString();
+
+                usersInfoList.Add(users);
+            }
+            con.Close();
+            return usersInfoList;
+
+        }
+    }
+}
--- UserFrontend/Pages/Index.cshtml.cs
+++ UserFrontend/Pages/Index.cshtml.cs
@@ -2,33 +2,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
+using UserFrontend.Models;
 
 namespace UserFrontend.Pages
 {
     public class IndexModel : PageModel
     {
-        public string firstName;
-        public string lastName;
-        public string email;
-        public string userId;
+        public List<Users> usersLists = new List<Users>();
+        //public string firstName;
+        //public string lastName;
+        //public string email;
+        //public string userId;
         //public string email;
 
         public void OnGet()
         {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog= FoodRescue_inlamning_2 ; Integrated Security=True";
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
-            string sqlQuery = "Select FirstName, LastName,Email,UserId From UserPrivateInfos Where UserId =2";
-            SqlCommand cmd = new SqlCommand(sqlQuery,con);
-            SqlDataReader dr = cmd.ExecuteReader();
-            if (dr.Read())
-            {
-                firstName = dr["FirstName"].ToString();
-                lastName= dr["lastName"].ToString();
-                email = dr["Email"].ToString();
-                userId = dr["UserId"].ToString();
-            }
-            con.Close();
+             Users users = new Users();
+            usersLists=users.GetUsersInfo();
+            //string connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog= FoodRescue_inlamning_2 ; Integrated Security=True";
+            //SqlConnection con = new SqlConnection(connectionString);
+            //con.Open();
+            //string sqlQuery = "Select FirstName, LastName,Email,UserId From UserPrivateInfos Where UserId =2";
+            //SqlCommand cmd = new SqlCommand(sqlQuery,con);
+            //SqlDataReader dr = cmd.ExecuteReader();
+            //if (dr.Read())
+            //{
+            //    firstName = dr["FirstName"].ToString();
+            //    lastName= dr["lastName"].ToString();
+            //    email = dr["Email"].ToString();
+            //    userId = dr["UserId"].ToString();
+            //}
+            //con.Close();
             
         }
         //public void OnGet()
