68cf691a412539ea96043deb41fa26bc462bf3e3
Haqjahirul123 <haqjahirul1983@gmail.com>
2021-12-03 11:29:40 +0100

    changes on program file

--- UserFrontend/Program.cs
+++ UserFrontend/Program.cs
@@ -1,36 +1,40 @@
 using DataLayer.Backend;
 using DataLayer.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-//var admin = new AdminBackend( );
-
-string databaseName = "FoodRescue_ProjectDatabase_REAL";
-var admin = new AdminBackend(databaseName);    //fix connection
-admin.CreateAndSeedDb();
-
-//using var ctx = new FoodRescue_DbContext();
-//Console.WriteLine(ctx);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static string DatabaseName = "FoodRescue_ProjectDatabase_REAL";
+    public static UserBackend User = new UserBackend(1, DatabaseName);
+
+    public static void Main(string[] args)
+    {
+        AdminBackend adminBackend = new(DatabaseName);
+        adminBackend.CreateDb();
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
