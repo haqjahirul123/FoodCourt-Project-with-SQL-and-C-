4eff92ef37a527e837defd459a2cbe7ed1829c02
Haqjahirul123 <haqjahirul1983@gmail.com>
2021-12-03 11:47:56 +0100

    redirect to edit page

--- UserFrontend/Pages/Edit.cshtml.cs
+++ UserFrontend/Pages/Edit.cshtml.cs
@@ -5,8 +5,16 @@ namespace UserFrontend.Pages
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        public UserBackend User { get; set; }
+
+        [BindProperty] public FoodBox FoodBox { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
         {
+            User = Program.User;
+            User.BuyFoodBox(id);
+
+            return RedirectToPage("./Index");
         }
     }
 }
