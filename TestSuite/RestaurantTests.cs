dd6b0562881cf0e49467afd5e065ccd7ba68a4e6
Hasan Jamous <hasan.jamous@learnet.se>
2021-12-05 12:52:52 +0100

    gjorde ett test som testar mina metoder i backend

--- TestSuite/RestaurantTests.cs
+++ TestSuite/RestaurantTests.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using DataLayer.Backend;
+using DataLayer.Data;
+using DataLayer.Model;
 using Xunit;
 
 namespace TestSuite
@@ -6,5 +11,19 @@ namespace TestSuite
     {
         /*Restaurant tests below*/
 
+        private const string DataBaseTesting = "FoodRescue_ProjectDatabase_TEST";
+
+        [Fact]
+        void CheckExceptionsFoodBoxes()
+        {
+            using var ctx = new FoodRescue_DbContext(DataBaseTesting);
+            var restaurant = new RestaurantBackend(1, databaseName: DataBaseTesting);
+
+            List<FoodBox> f;
+
+            Assert.ThrowsAny<Exception>(() => f = restaurant.GetAllFoodBoxes());
+            Assert.ThrowsAny<Exception>(() => f = restaurant.GetSoldFoodBoxes());
+            Assert.ThrowsAny<Exception>(() => f = restaurant.GetUnsoldFoodBoxes());
+        }
     }
 }
